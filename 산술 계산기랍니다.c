def5107da30705e843ce75f92b60208821106255
jun hwi <112397463+Junhwipark@users.noreply.github.com>
2023-04-15 15:56:10 +0900

    Add files via upload

---
+++ 산술 계산기랍니다.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(void) {
+
+	char op;
+	int x, y, result;
+
+	printf("수식을 입력하세요:");
+	scanf_s("%d%c%d", x, op, y);
+
+	switch (op)
+	{
+	case'+':
+		result=x + y;
+	case'-':
+		result=x - y;
+	case'*':
+		result=x * y;
+	case'/':
+		result=x / y;
+	case'%':
+		result=x % y;
+
+	default:
+		printf("계산 할수없습니다.");
+
+		break;
+	}
+
+	printf("%d입니다.", &result);
+
+	return 0;
+}
