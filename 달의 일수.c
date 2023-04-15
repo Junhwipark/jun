aa9b45b78986bce594806a5e2b854a1e3ca32aaa
jun hwi <112397463+Junhwipark@users.noreply.github.com>
2023-04-15 14:41:59 +0900

    Add files via upload

---
+++ 달의 일수.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int month, days;
+
+	printf("달을 입력하세요:");
+	scaf_s("%d", &month);
+
+	switch (month)
+	{
+	case 2:
+		days = 28;
+		break;
+	case 4:
+	case 6:
+	case 8:
+	case 10:
+	case 12:
+		days = 30;
+		break;
+	default:
+		days = 31;
+		break;
+	}
+
+	printf("입력하신 달의 일수는:%d", &days);
+
+	return 0;
+
+}
